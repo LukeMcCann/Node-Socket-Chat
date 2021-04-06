5ec77bf41dadea8a5776b25744f15aa11c3f3bc1
LukeMcCann <developer-lukemccann@outlook.com>
2021-04-06 20:39:50 +0100

    Add basic message sending

--- server.js
+++ server.js
@@ -4,5 +4,9 @@ const io = require('socket.io')(3000);
 
 io.on('connection', socket => {
     socket.emit('chat-message', 'Hello World');
+
+    socket.on('send-chat-message', message => {
+        console.log(message);
+    });
 });
 
