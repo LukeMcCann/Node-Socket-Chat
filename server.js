fe44c8bef2ad57b850ec0125b23eb2881cbadd91
LukeMcCann <developer-lukemccann@outlook.com>
2021-04-06 20:54:16 +0100

    Add basic message hadnling

--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 
 const io = require('socket.io')(3000);
 
-io.on('connection', socket => {
-    socket.emit('chat-message', 'Hello World');
+const users = {}
 
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-connected', name);
+    });
     socket.on('send-chat-message', message => {
-        console.log(message);
+        socket.broadcast.emit('chat-message', message);
     });
 });
 
